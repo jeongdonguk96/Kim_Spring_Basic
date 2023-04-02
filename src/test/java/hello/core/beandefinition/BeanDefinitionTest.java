701291f1c8247234ad396ea17bcdffdec01f0796
jungdonguk <foodsmith96@naver.com>
2023-04-02 23:04:21 +0900

    김영한 스프링 기본편 0.5v

---
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,26 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplictionBean() {
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.print("beanDefinition = " + beanDefinition);
+                System.out.println("beanDefinitionName = " + beanDefinitionName);
+            }
+        }
+    }
+}
